0735c131e0a193791ca0e81e11f5d84f8c88a30b
sunjianrong <sunybyjava@gmail.com>
2017-09-03 12:25:37 +0800

    修正loginRequireInterceptor拦截器中未添加拦截的地址,导致用户退出后引发循环请求重新登录的验证bug

--- src/main/java/com/suny/configuration/WendaWebConfiguration.java
+++ src/main/java/com/suny/configuration/WendaWebConfiguration.java
@@ -27,7 +27,7 @@ public class WendaWebConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(passportInterceptor);
-//        registry.addInterceptor(loginRequiredInterceptor);
+        registry.addInterceptor(loginRequiredInterceptor).addPathPatterns("/user/**");
         super.addInterceptors(registry);
     }
 }
